17b7734d73b91a9c15c84ee79662fdfcc617f79a
zakaria-samlali <samlali.zakaria@gmail.com>
2024-01-24 12:12:19 +0100

    init

---
+++ src/main/java/model/Todo.java
@@ -0,0 +1,23 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String task;
+    private Date date;
+    private boolean isDone;
+
+}
